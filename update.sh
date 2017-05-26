c9bdd08dcf37497e7c31868fa932ce9088c046bd
Ghiroz <runozo@gmail.com>
2017-05-27 01:03:23 +0200

    extended shell script

--- update.sh
+++ update.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
+python -m venv env
 source ./env/bin/activate
-python get_my_playlist.py
+pip install -r requirements.txt
+USER="xxx"
+PASSWORD="xxx"
+PHONE_ID="xxx"
+python get_my_playlist.py $USER $PASSWORD $PHONE_ID
 git commit -am "`date`"
 git push origin master
